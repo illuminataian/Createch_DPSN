0ec61adf482f87862ea1deb35729588a742e0cc7
illuminataian <72221159+illuminataian@users.noreply.github.com>
2021-09-12 15:35:45 +0530

    UI updated

--- createch_dpsn/lib/screens/select.dart
+++ createch_dpsn/lib/screens/select.dart
@@ -30,29 +30,37 @@ class _SelectPageState extends State<SelectPage> {
         backgroundColor: Colors.grey[850],
         foregroundColor: Colors.grey[100],
       ),
-      body: ListView.builder(
-          itemCount: info.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-              child: Card(
-                child: ListTile(
-                    title: Text(info[index].text),
-                    onTap: () {
-                      Navigator.pushReplacementNamed(
-                        context,
-                        "/",
-                      );
-                      setState(() {
-                        globals.name_city = info[index].text;
-                        globals.pollution = info[index].strength;
-                        globals.name_country = info[index].country;
-                      });
-                    }),
-              ),
-            );
-          }),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.grey[700]),
+        child: ListView.builder(
+            itemCount: info.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+                child: Card(
+                  elevation: 10,
+                  color: Colors.grey.shade800,
+                  child: ListTile(
+                      title: Text(
+                        info[index].text,
+                        style: TextStyle(color: Colors.grey.shade100),
+                      ),
+                      onTap: () {
+                        Navigator.pushReplacementNamed(
+                          context,
+                          "/",
+                        );
+                        setState(() {
+                          globals.name_city = info[index].text;
+                          globals.pollution = info[index].strength;
+                          globals.name_country = info[index].country;
+                        });
+                      }),
+                ),
+              );
+            }),
+      ),
     );
   }
 }
